1e67f1afc48a042375a6d70b1500e3918ef9ee0d
sstefdev <sstefandev@gmail.com>
2021-03-21 22:33:30 +0100

    Added collision to the Flappy asset, added generation of Pipes and added gameOver func

--- game.js
+++ game.js
@@ -1,10 +1,12 @@
 let stage, loader, flappy;
+let started = false;
+let polygon;
 
 const init = () => {
-  stage = new createjs.StageGL("gameCanvas");
+  stage = new createjs.Stage("gameCanvas");
 
   createjs.Ticker.timingMode = createjs.Ticker.RAF_SYNCED;
-  createjs.Ticker.framerate = 60;
+  createjs.Ticker.framerate = 120;
   createjs.Ticker.addEventListener("tick", stage);
 
   const background = new createjs.Shape();
@@ -39,8 +41,10 @@ const init = () => {
 const handleComplete = () => {
   createClouds();
   createFlappy();
-  //   createPipes();
   stage.on("stagemousedown", jumpFlappy);
+  createjs.Ticker.addEventListener("tick", checkCollision);
+  polygon = new createjs.Shape();
+  stage.addChild(polygon);
 };
 
 const createClouds = () => {
@@ -87,11 +91,14 @@ const createFlappy = () => {
   flappy.regY = flappy.image.height / 2;
   flappy.x = stage.canvas.width / 2;
   flappy.y = stage.canvas.height / 2;
-  flappySize(flappy);
+  //   flappySize(flappy);
   stage.addChild(flappy);
 };
 
 const jumpFlappy = () => {
+  if (!started) {
+    startGame();
+  }
   createjs.Tween.get(flappy, { override: true })
     .to(
       {
@@ -109,11 +116,84 @@ const jumpFlappy = () => {
     .call(gameOver);
 };
 
-// const createPipes = () => {
-//     let topPipe, bottomPipe;
-//     let
-// }
+const pipeSize = (botPipe, topPipe) => {
+  botPipe.scaleX = 0.35;
+  botPipe.scaleY = 0.35;
+  topPipe.scaleX = 0.35;
+  topPipe.scaleY = 0.35;
+};
+
+const createPipes = () => {
+  let topPipe, bottomPipe;
+  let position = Math.floor(Math.random() * 280 + 100);
+
+  topPipe = new createjs.Bitmap(loader.getResult("pipe"));
+  topPipe.y = position - 75;
+  topPipe.x = stage.canvas.width + topPipe.image.width / 2;
+  topPipe.rotation = 180;
+  topPipe.skewY = 180;
+  topPipe.name = "pipe";
+
+  bottomPipe = new createjs.Bitmap(loader.getResult("pipe"));
+  bottomPipe.y = position + 75;
+  bottomPipe.x = stage.canvas.width + bottomPipe.image.width / 2;
+  bottomPipe.name = "pipe";
+
+  topPipe.regX = bottomPipe.regX = topPipe.image.width / 2;
+
+  createjs.Tween.get(topPipe)
+    .to({ x: 0 - topPipe.image.width }, 15000)
+    .call(() => removePipe(topPipe));
+
+  createjs.Tween.get(bottomPipe)
+    .to({ x: 0 - bottomPipe.image.width }, 15000)
+    .call(() => removePipe(bottomPipe));
+
+  pipeSize(bottomPipe, topPipe);
+
+  stage.addChild(bottomPipe, topPipe);
+};
+
+const removePipe = (pipe) => {
+  stage.removeChild(pipe);
+};
+
+const startGame = () => {
+  started = true;
+  createPipes();
+  setInterval(createPipes, 2000);
+};
+
+const checkCollision = () => {
+  let leftX = flappy.x - flappy.regX + 5;
+  let leftY = flappy.y - flappy.regY + 5;
+  let points = [
+    new createjs.Point(leftX, leftY),
+    new createjs.Point(leftX + flappy.image.width - 10, leftY),
+    new createjs.Point(leftX, leftY + flappy.image.height - 10),
+    new createjs.Point(
+      leftX + flappy.image.width - 10,
+      leftY + flappy.image.height - 10
+    ),
+  ];
+
+  polygon.graphics.clear().beginStroke("black");
+  polygon.graphics
+    .moveTo(points[0].x, points[0].y)
+    .lineTo(points[2].x, points[2].y)
+    .lineTo(points[3].x, points[3].y)
+    .lineTo(points[1].x, points[1].y)
+    .lineTo(points[0].x, points[0].y);
+
+  for (let i = 0; i < points.length; i++) {
+    let objects = stage.getObjectsUnderPoint(points[i].x, points[i].y);
+    if (objects.filter((object) => object.name === "pipe").length > 0) {
+      gameOver();
+      return;
+    }
+  }
+};
 
 const gameOver = () => {
-  console.log("GAME OVER!");
+  createjs.Tween.removeAllTweens();
 };
